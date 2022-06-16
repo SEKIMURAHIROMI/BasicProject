02b8a75bc9255b9f54e5186080f07fc1dc20e2dc
Hiromi Sekimura <t-hsekimura@microsoft.com>
2022-06-17 08:36:56 +0900

    urlpyにfrom django.conf.urls import include, url import HelloDjangoApp.views
    # Django processes URL patterns in the order they appear in the array
    urlpatterns = [
        url(r'^$', HelloDjangoApp.views.index, name='index'),
        url(r'^home$', HelloDjangoApp.views.index, name='home'),
    ]いれました

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -8,6 +8,15 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 
+from django.conf.urls import include, url
+import HelloDjangoApp.views
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+]
+
 
 urlpatterns = [
     path('', views.home, name='home'),
